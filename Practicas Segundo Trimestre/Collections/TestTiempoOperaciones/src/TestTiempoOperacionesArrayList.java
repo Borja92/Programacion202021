e49e2b5027dbed69b5e6060a6f1f1e2811b6f57f
Borja Badía <44509208+Borja92@users.noreply.github.com>
2021-02-02 18:56:02 +0100

    up

--- Practicas Segundo Trimestre/Collections/TestTiempoOperaciones/src/TestTiempoOperacionesArrayList.java
+++ Practicas Segundo Trimestre/Collections/TestTiempoOperaciones/src/TestTiempoOperacionesArrayList.java
@@ -6,18 +6,21 @@ public class TestTiempoOperacionesArrayList {
     public final static int MITAD_LISTA = 1;
 
     private ArrayList<Integer> arrayList = new ArrayList<>();
+    private LinkedList<Integer> linkedList = new LinkedList<>();
 
     Random rdn = new Random();
     Date date = new Date();
     long instanteInicio = System.currentTimeMillis();
 
 
-    public TestTiempoOperacionesArrayList(ArrayList<Integer> arrayList) {
+    public TestTiempoOperacionesArrayList() {
+
         this.arrayList = arrayList;
+        this.linkedList= linkedList;
+
     }
 
-    public TestTiempoOperacionesArrayList() {
-    }
+
 
     public long insertarElementos(int numElementos, int posicion) {
 //Inserción en el principio
@@ -105,7 +108,7 @@ numElementos--;
 
     @Override
     public String toString() {
-        return "TestTiempoOperacionesArrayList{" +
+        return "TestTiempoOperacionesLinkedList{" +
                 "arrayList=" + arrayList +
                 '}';
     }
@@ -113,31 +116,46 @@ numElementos--;
     public static void main(String[] args) {
 
         TestTiempoOperacionesArrayList testTiempoOperacionesArrayList = new TestTiempoOperacionesArrayList();
-
+        TestTiempoOperacionesLinkedList testTiempoOperacionesLinkedList = new TestTiempoOperacionesLinkedList();
         System.out.println("Tiempo en milisegundos inserción al principio del Array List: " + testTiempoOperacionesArrayList.insertarElementos(15, INICIO_LISTA));
         System.out.println();
+        System.out.println("Tiempo en milisegundos inserción al principio del LinkedList: " + testTiempoOperacionesLinkedList.insertarElementosLinkedList(15, INICIO_LISTA));
+        System.out.println();
+
         System.out.println(testTiempoOperacionesArrayList.toString());
+        System.out.println(testTiempoOperacionesLinkedList);
         System.out.println();
         System.out.println("Tiempo en milisegundos inserción en mitad del Array List: " + testTiempoOperacionesArrayList.insertarElementos(5, MITAD_LISTA));
+        System.out.println("Tiempo en milisegundos inserción en mitad del Linked List: " + testTiempoOperacionesLinkedList.insertarElementosLinkedList(5, MITAD_LISTA));
         System.out.println();
         System.out.println(testTiempoOperacionesArrayList.toString());
+        System.out.println(testTiempoOperacionesLinkedList);
         System.out.println();
         System.out.println("Tiempo en milisegundos recorrer arrayList: " + testTiempoOperacionesArrayList.recorrerLista());
+        System.out.println("Tiempo en milisegundos recorrer Linked List: " + testTiempoOperacionesLinkedList.recorrerListaLinkedList());
         System.out.println();
         System.out.println("Tiempo de ArrayList de ordenador de menor a mayor: " + testTiempoOperacionesArrayList.ordenarLista());
+        System.out.println("Tiempo de LinkedList de ordenador de menor a mayor: " + testTiempoOperacionesLinkedList.ordenarListaLinkedList());
         System.out.println();
         System.out.println("Array List Ordenado de mayor a menor: " + testTiempoOperacionesArrayList.toString());
+        System.out.println("Linked List Ordenado de mayor a menor: " + testTiempoOperacionesLinkedList);
         System.out.println();
         System.out.println("Tiempo en milisegundos que tarda en borrar N elementos en un ArrayList desde el inicio: "+testTiempoOperacionesArrayList.borrarElementos(10,INICIO_LISTA));
+        System.out.println("Tiempo en milisegundos que tarda en borrar N elementos en un LinkedList desde el inicio: "+testTiempoOperacionesLinkedList.borrarElementosLinkedList(10,INICIO_LISTA));
         System.out.println();
         System.out.println(testTiempoOperacionesArrayList.toString());
+        System.out.println(testTiempoOperacionesLinkedList);
         System.out.println();
         System.out.println("Tiempo en milisegundos que tarda en borrar N elementos en un ArrayList desde la mitad: "+testTiempoOperacionesArrayList.borrarElementos(6,MITAD_LISTA));
+        System.out.println("Tiempo en milisegundos que tarda en borrar N elementos en un Linked List desde la mitad: "+testTiempoOperacionesLinkedList.borrarElementosLinkedList(6,MITAD_LISTA));
         System.out.println();
         System.out.println(testTiempoOperacionesArrayList.toString());
+        System.out.println(testTiempoOperacionesLinkedList);
         System.out.println("Tiempo en milisegundos que tarda en borrar N elementos en un ArrayList desde el inicio: "+testTiempoOperacionesArrayList.borrarElementos(2,MITAD_LISTA));
+        System.out.println("Tiempo en milisegundos que tarda en borrar N elementos en un Linked List desde el inicio: "+testTiempoOperacionesLinkedList.borrarElementosLinkedList(2,MITAD_LISTA));
         System.out.println();
         System.out.println(testTiempoOperacionesArrayList.toString());
+        System.out.println(testTiempoOperacionesLinkedList);
 
     }
 }
---
+++ Practicas Segundo Trimestre/Collections/TestTiempoOperaciones/src/TestTiempoOperacionesLinkedList.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+public class TestTiempoOperacionesLinkedList{
+        public final static int INICIO_LISTA = 0;
+        public final static int MITAD_LISTA = 1;
+
+        private LinkedList<Integer> linkedList = new LinkedList<>();
+
+        Random rdn = new Random();
+        Date date = new Date();
+        long instanteInicio = System.currentTimeMillis();
+
+
+        public TestTiempoOperacionesLinkedList() {
+
+            this.linkedList= linkedList;
+
+        }
+
+
+
+        public long insertarElementosLinkedList(int numElementos, int posicion) {
+//Inserción en el principio
+            rdn.setSeed(date.getTime());
+            if (posicion == INICIO_LISTA) {
+                for (int i = 0; i < numElementos; i++) {
+                    linkedList.add(posicion, rdn.nextInt(5000));
+                }
+            }
+//Inserción en la mitad
+            if (posicion == MITAD_LISTA) {
+                posicion = linkedList.size() / 2;
+
+                for (int i = 0; i < numElementos; i++) {
+                    linkedList.add(posicion, rdn.nextInt(5000));
+                }
+            }
+            return System.currentTimeMillis() - instanteInicio;
+        }
+
+        public long recorrerListaLinkedList() {
+
+
+            Iterator<Integer> it = linkedList.iterator();
+            while (it.hasNext()) {
+                System.out.print("[" + it.next() + "]");
+            }
+            System.out.println();
+
+            return System.currentTimeMillis() - instanteInicio;
+        }
+
+        public long ordenarListaLinkedList() {
+            Collections.sort(linkedList);
+            return System.currentTimeMillis() - instanteInicio;
+        }
+
+        public long borrarElementosLinkedList(int numElementos, int posicion) {
+            // Donde posicion = INICIO_LISTA | MITAD_LISTA
+
+            //Eliminar desde  el principio
+
+            if (posicion == INICIO_LISTA) {
+
+                Iterator<Integer> it=linkedList.iterator();
+                while (it.hasNext()&numElementos>0){
+                    linkedList.remove(linkedList.get(posicion));
+                    numElementos--;
+                }
+
+            }
+//Eliminar desde la mitad
+            if (posicion == MITAD_LISTA) {
+                posicion = linkedList.size() /2;
+
+                if (posicion%2==0){
+                    Iterator<Integer> it=linkedList.iterator();
+
+                    while (it.hasNext()&&numElementos>0){
+
+                        linkedList.remove(linkedList.get(posicion));
+                        numElementos--;
+                    }}
+                else {
+                    //Si posición es impar le resto uno, para volverlo par y así poder dividir el array en dos y proceder a borrarlo n veces
+                    posicion-=1;
+                    Iterator<Integer> it=linkedList.iterator();
+
+                    while (it.hasNext()&&numElementos>0){
+
+                        linkedList.remove(linkedList.get(posicion));
+                        numElementos--;
+                    }
+                }}
+
+            return System.currentTimeMillis() - instanteInicio;
+        }
+
+    @Override
+    public String toString() {
+        return "TestTiempoOperacionesLinkedList{" +
+                "linkedList=" + linkedList +
+                '}';
+    }
+}
+
+
