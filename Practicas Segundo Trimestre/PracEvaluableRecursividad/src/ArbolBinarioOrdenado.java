8ef2129f9703d140fc02576f3781abc1b3d536fd
Borja Badía <44509208+Borja92@users.noreply.github.com>
2021-02-05 20:49:01 +0100

    arbolNodosBinario

--- Practicas Segundo Trimestre/PracEvaluableRecursividad/src/ArbolBinarioOrdenado.java
+++ Practicas Segundo Trimestre/PracEvaluableRecursividad/src/ArbolBinarioOrdenado.java
@@ -1,42 +1,222 @@
-/*
+/**
+        *
+        *Practica Evaluable sobre recursividad
+
+        ---Árbol de Nodos---
+
+        Autor:                 A. Borja Badía Checa
+
+        *Instrucciones:
+        *
+        * 1. Escribe el método addValor, para que el nuevo nodo se inserte correctamente en el árbol.
+        * 2. Escribe el método inArbol que comprueba si un cierto valor está presente en el árbol.
+        * 3. Escribe el método "recorridoEnOrden" que realiza el recorrido en orden de un árbol binario.
+        */
+
 public class ArbolBinarioOrdenado {
     private int valor;
-    private ArbolBinarioOrdenado hijoIzquierdo, hijoderecho, arbol;
+    private ArbolBinarioOrdenado hijoIzquierdo, hijoDerecho, arbol;
 
-    public ArbolBinarioOrdenado() {
+    /**
+     * Declaración constructor
+     */
+    public ArbolBinarioOrdenado(int valor) {
         this.valor = valor;
         this.hijoIzquierdo = null;
-        this.hijoderecho = null;
+        this.hijoDerecho = null;
         this.arbol = null;
     }
 
 
-    void addValor(int valor){
+    /** Fin declaración construtor
+     */
 
-        ArbolBinarioOrdenado nodoNuevo = new ArbolBinarioOrdenado();
+    /**
+     * Declaración de Getters&Setters
+     */
+    public int getValor() {
+        return valor;
+    }
 
-        nodoNuevo.valor=valor;
-        nodoNuevo.hijoderecho=null;
-        nodoNuevo.hijoIzquierdo=null;
-        if (arbol==null){
-            arbol=nodoNuevo;
-        } else {
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
 
-            ArbolBinarioOrdenado nodoAnterior=null, recorrer;
-            recorrer=arbol;
-            while (recorrer!=null){
-                    nodoAnterior=recorrer;
-                if (valor<recorrer.valor){
+    public ArbolBinarioOrdenado getHijoIzquierdo() {
+        return hijoIzquierdo;
+    }
 
-                } else{
+    public void setHijoIzquierdo(ArbolBinarioOrdenado hijoIzquierdo) {
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
+
+    public ArbolBinarioOrdenado getHijoDerecho() {
+        return hijoDerecho;
+    }
+
+    public void setHijoDerecho(ArbolBinarioOrdenado hijoDerecho) {
+        this.hijoDerecho = hijoDerecho;
+    }
+
+    public ArbolBinarioOrdenado getArbol() {
+        return arbol;
+    }
+
+    public void setArbol(ArbolBinarioOrdenado arbol) {
+        this.arbol = arbol;
+    }
+
+/** Fin declaración Getters&Setters
+ */
+    /**
+     * Inicio declaración de métodos de la Clase Nodo
+     */
 
 
-                }
+    public  String pintarValor(){
+       /* int casoBase=0;
+        int menorCasoBase;
+        int mayorCasoBase;
 
-            }
+        if(valor>casoBase){
+            mayorCasoBase=valor;
+            System.out.print(mayorCasoBase + "\\");
         }
+        if (valor<casoBase){
+            menorCasoBase=valor;
+            System.out.print(menorCasoBase + "/");
+        } else         System.out.println("/" + valor + "\\");
+*/
+        //   pintar("", this, false);
+        return valor+"";
 
     }
 
+    public void addValor(int valor) {
+        // Método recursivo:
+        // Si valor < this.valor
+        // añadir en el subárbol izquierdo*
+        // En caso contrario
+        // añadir en el subárbol derecho
+        //
+        // * Un subárbol es un nodo que apunta a otros.
+        // ** El caso base es un subárbol nulo.
+
+        if (valor<this.valor){
+            if (hijoIzquierdo==null) {
+                this.setHijoIzquierdo(new ArbolBinarioOrdenado(valor));
+            }else
+                this.hijoIzquierdo.addValor(valor);
+        }
+        else if (valor>this.valor){
+            if (hijoDerecho==null) {
+                this.setHijoDerecho(new ArbolBinarioOrdenado(valor));
+            }else
+                this.hijoDerecho.addValor(valor);
+        }
+        }
+
+
+
+    public boolean inArbol(int valor){
+        // Método recursivo:
+        //
+        // Un valor está en el árbol si está
+        // en el nodo actual o bien en alguno de sus hijos.
+        // El caso base es:
+        // - Valor encontrado -> true
+        // - Nodo nulo -> false
+
+        boolean valorEnArbol=false;
+        if (valor == this.valor) valorEnArbol=true;
+        else if (valor < this.valor) {
+            if (hijoIzquierdo == null) valorEnArbol= false;
+            else return hijoIzquierdo.inArbol(valor);
+        } else if (valor > this.valor) {
+            if (hijoDerecho == null) valorEnArbol=false;
+            else return hijoDerecho.inArbol(valor);
+        }
+        return valorEnArbol;
+
+    }
+
+//Nodo Izq -> Nodo Contenedor -> Nodo Der
+
+    public String recorridoEnOrden() {
+        // La cadena devuelta es la concatenación en orden
+        // de las siguientes cadenas:
+        //
+        // - La cadena correspondiente al hijo izquierdo
+        // - El valor del nodo
+        // - La cadena correspondiente al hijo derecho.
+        //
+        // El caso base es:
+        // - Un nodo hijo vacío -> ""
+        String resultado="";
+
+        if (hijoIzquierdo!=null) {
+            resultado+=hijoIzquierdo.recorridoEnOrden() + "";
+
+        }
+
+        resultado+=this.pintarValor();
+
+        if (hijoDerecho!=null) {
+            resultado+=hijoDerecho.recorridoEnOrden();
+        }
+    return resultado;
+    }
+
+
+
+   /* public void pintar (String prefijo, ArbolBinarioOrdenado nodo, boolean left) {
+        if (nodo != null) {
+            System.out.println(prefijo + (left ? "└── " : "├──") + nodo.valor);
+            recorridoEnOrden();
+            pintar(prefijo + (left ? "|   " : "    "), nodo.hijoIzquierdo, true);
+            pintar(prefijo + (left ? "|   " : "    "), nodo.hijoDerecho, false);
+
+        }
+ }*/
+    @Override
+    public String toString() {
+        return "\nArbolBinarioOrdenado{" +
+                "valor=" + valor +
+                ", hijoIzquierdo=" + hijoIzquierdo +
+                ", hijoDerecho=" + hijoDerecho +
+                ", arbol=" + arbol +
+                '}';
+    }
+    public static void main(String[] args) {
+
+        ArbolBinarioOrdenado arbolBinarioOrdenado= new ArbolBinarioOrdenado(0);
+
+        arbolBinarioOrdenado.addValor(0);
+        arbolBinarioOrdenado.addValor(1);
+        arbolBinarioOrdenado.addValor(-1);
+        arbolBinarioOrdenado.addValor(8);
+        arbolBinarioOrdenado.addValor(-2);
+        arbolBinarioOrdenado.addValor(9);
+        arbolBinarioOrdenado.addValor(10);
+        arbolBinarioOrdenado.addValor(11);
+        arbolBinarioOrdenado.addValor(-88);
+        arbolBinarioOrdenado.addValor(15);
+        arbolBinarioOrdenado.addValor(16);
+        arbolBinarioOrdenado.addValor(10);
+        arbolBinarioOrdenado.addValor(12);
+        arbolBinarioOrdenado.addValor(5);
+        arbolBinarioOrdenado.addValor(27);
+        arbolBinarioOrdenado.addValor(26);
+
+        System.out.println(arbolBinarioOrdenado);
+
+        arbolBinarioOrdenado.recorridoEnOrden();
+        System.out.println();
+
+     //   arbolBinarioOrdenado.pintarValor();
+
+
+    }
+
+
 }
-*/
--- Practicas Segundo Trimestre/PracEvaluableRecursividad/src/Nodo.java
+++ Practicas Segundo Trimestre/PracEvaluableRecursividad/src/Nodo.java
@@ -1,16 +1,15 @@
 /**
-*
-*Practica Evaluable sobre recursividad
-
-                            ---Árbol de Nodos---
-
- Autor:                 A. Borja Badía Checa
-
- *Instrucciones:
+ * Practica Evaluable sobre recursividad
+ *
+ *                             ---Árbol de Nodos Binario---
+ *
+ * Autor:                 A. Borja Badía Checa
+ *
+ * Instrucciones:
  *
  * 1. Escribe el método addValor, para que el nuevo nodo se inserte correctamente en el árbol.
  * 2. Escribe el método inArbol que comprueba si un cierto valor está presente en el árbol.
- * 3. Escribe el método "recorridoEnOrdenIzqDerech" que realiza el recorrido en orden de un árbol binario.
+ * 3. Escribe el método "recorridoEnOrden" que realiza el recorrido en orden de un árbol binario.
  */
 
 public class Nodo {
@@ -19,9 +18,9 @@ public class Nodo {
     private Nodo hijoIzquierdo;
     private Nodo hijoDerecho;
 
-/**
+    /**
      * Declaración constructor
-   */
+     */
 
 
     public Nodo(int valor) {
@@ -30,13 +29,14 @@ public class Nodo {
         this.hijoDerecho = null;
     }
 
-/** Fin declaración construcor
-
-
-
-/**
+    /**
+     * Fin declaración construtor
+     * <p>
+     * <p>
+     * <p>
+     * /**
      * Declaración de Getters&Setters
-   */
+     */
 
 
     public int getValor() {
@@ -64,11 +64,11 @@ public class Nodo {
     }
 
 /** Fin declaración Getters&Setters
-*/
+ */
 
-/**
+    /**
      * Inicio declaración de métodos de la Clase Nodo
-   */
+     */
 
 
     public void addValor(int valor) {
@@ -80,22 +80,22 @@ public class Nodo {
         //
         // * Un subárbol es un nodo que apunta a otros.
         // ** El caso base es un subárbol nulo.
-        if(valor<this.valor){
-            if (hijoIzquierdo!=null){
+        if (valor < this.valor) {
+            if (hijoIzquierdo != null) {
                 hijoIzquierdo.addValor(valor);
             } else {
-                hijoIzquierdo= new Nodo(valor);
-        }
+                hijoIzquierdo = new Nodo(valor);
+            }
         } else {
-            if (hijoDerecho!=null){
+            if (hijoDerecho != null) {
                 hijoDerecho.addValor(valor);
             } else {
-                hijoDerecho=new Nodo(valor);
+                hijoDerecho = new Nodo(valor);
             }
-            }
-}
+        }
+    }
 
-    public boolean inArbol(int valor){
+    public boolean inArbol(int valor) {
         // Método recursivo:
         //
         // Un valor está en el árbol si está
@@ -103,13 +103,13 @@ public class Nodo {
         // El caso base es:
         // - Valor encontrado -> true
         // - Nodo nulo -> false
-    boolean valorEnArbol=false;
-if (valor == this.valor) valorEnArbol=true;
+        boolean valorEnArbol = false;
+        if (valor == this.valor) valorEnArbol = true;
         else if (valor < this.valor) {
-            if (hijoIzquierdo == null) valorEnArbol= false;
+            if (hijoIzquierdo == null) valorEnArbol = false;
             else return hijoIzquierdo.inArbol(valor);
         } else if (valor > this.valor) {
-            if (hijoDerecho == null) valorEnArbol=false;
+            if (hijoDerecho == null) valorEnArbol = false;
             else return hijoDerecho.inArbol(valor);
         }
         return valorEnArbol;
@@ -118,7 +118,7 @@ if (valor == this.valor) valorEnArbol=true;
 
 //Nodo Izq -> Nodo Contenedor -> Nodo Der
 
-    public void recorridoEnOrdenIzqDerech() {
+    public void recorridoEnOrden() {
         // La cadena devuelta es la concatenación en orden
         // de las siguientes cadenas:
         //
@@ -129,86 +129,83 @@ if (valor == this.valor) valorEnArbol=true;
         // El caso base es:
         // - Un nodo hijo vacío -> ""
         if (hijoIzquierdo != null) {
-            hijoIzquierdo.recorridoEnOrdenIzqDerech();
+            if (hijoIzquierdo.valor < this.valor) {
+                hijoIzquierdo.recorridoEnOrden();
+            }
         }
-        if (hijoIzquierdo != null) {
-            hijoIzquierdo.recorridoEnOrdenIzqDerech();
-        }
-pintarValor();
-    }
+        pintarValor();
 
-    //Nodo Árbol -> Nodo Izq -> Nodo Derecho
-
-    public  void recorridoEnOrdenArbolIzqDerech(){
-pintarValor();
-        if (hijoIzquierdo!=null){
-            hijoIzquierdo.recorridoEnOrdenArbolIzqDerech();
-        }
-        if (hijoDerecho!=null){
-            hijoDerecho.recorridoEnOrdenArbolIzqDerech();
+        if (hijoDerecho != null) {
+            if (hijoDerecho.valor > this.valor) {
+                hijoDerecho.recorridoEnOrden();
+            }
         }
     }
 
-    public  void pintarValor(){
-        int casoBase=0;
-        int menorCasoBase=0;
-        int mayorCasoBase=0;
 
-        if(valor>casoBase){
-            mayorCasoBase=valor;
+    public void pintarValor() {
+        int casoBase = 0;
+        int menorCasoBase;
+        int mayorCasoBase;
+
+        if (valor > casoBase) {
+            mayorCasoBase = valor;
             System.out.println(mayorCasoBase + "\\");
         }
-        if (valor<casoBase){
-            menorCasoBase=valor;
+        if (valor < casoBase) {
+            menorCasoBase = valor;
             System.out.println(menorCasoBase + "/");
-        } else         System.out.println("/" + valor + "\\");
+        }
+
     }
+
     @Override
     public String toString() {
-        return "Nodo{" +
-                "valor=" + valor +
-                ", hijoIzquierdo=" + hijoIzquierdo +
-                ", hijoDerecho=" + hijoDerecho +
+        return "\nNodo{" +
+                "Valor=" + valor +
+                ", HijoIzquierdo=" + hijoIzquierdo +
+                ", HijoDerecho=" + hijoDerecho +
                 '}';
     }
 
-/** Fin declaración de métodos de la clase Nodo
-*/
+    /**
+     * Fin declaración de métodos de la clase Nodo
+     */
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Nodo arbol = new Nodo(0);
+        Nodo arbol = new Nodo(0);
 
-    //Añadir elementos al árbol
-    arbol.addValor(1);
-    arbol.addValor(2);
-    arbol.addValor(3);
-    arbol.addValor(4);
-    arbol.addValor(5);
-    arbol.addValor(6);
-    arbol.addValor(7);
-    arbol.addValor(8);
-    arbol.addValor(9);
-    arbol.addValor(10);
-    arbol.addValor(-5);
-    arbol.addValor(-17);
-    arbol.addValor(-50);
-    arbol.addValor(-88);
+        //Añadir elementos al árbol
+        arbol.addValor(1);
+        arbol.addValor(2);
+        arbol.addValor(-4);
+        arbol.addValor(4);
+        arbol.addValor(5);
+        arbol.addValor(18);
+        arbol.addValor(7);
+        arbol.addValor(8);
+        arbol.addValor(9);
+        arbol.addValor(10);
+        arbol.addValor(-5);
+        arbol.addValor(-17);
+        arbol.addValor(-50);
+        arbol.addValor(-4);
+        arbol.addValor(3);
+        arbol.addValor(-2);
+        arbol.addValor(-1);
+        arbol.addValor(5);
 //Imprimir el árbol
-    System.out.println(arbol);
-    System.out.println();
-    //Comprobar el método que inArbol
-    System.out.println("El valor 7 se encuentra en el árbol: "+arbol.inArbol(7));
-    System.out.println("El valor 3 se encuentra en el árbol: "+arbol.inArbol(3));
-    System.out.println("El valor 99 se encuentra en el árbol: "+arbol.inArbol(99));
-
-    //Recorrer arbol y mostrar valores menores que el caso base
-    System.out.println("Nodos menores que el caso base: ");
-    arbol.recorridoEnOrdenIzqDerech();
-    System.out.println();
-    //Recorrer arbol y mostrar valores mayores que el caso base
-    System.out.println("Nodos mayores que el caso base: ");
-    arbol.recorridoEnOrdenArbolIzqDerech();
-
-}
+        System.out.println();
+        System.out.println(arbol); //Al imprimir el árbol tal cual se aprecia cómo desde la semilla, se reproducen los nodos, los cuales a su vez tienen hojas con otros valores o están vacíos(null)
+        System.out.println();
+        //Comprobar el método  inArbol
+        System.out.println("El valor 7 se encuentra en el árbol: " + arbol.inArbol(7));
+        System.out.println("El valor 3 se encuentra en el árbol: " + arbol.inArbol(3));
+        System.out.println("El valor 99 se encuentra en el árbol: " + arbol.inArbol(99));
+        System.out.println();
+        //Recorrer arbol y mostrar nodos en orden <=0=>. Izquierda /(<) para valores menores,  drecha\(>) para valores mayores
+        System.out.println("=====Imprimir árbol ordenado=====");
+        arbol.recorridoEnOrden();
+    }
 }
