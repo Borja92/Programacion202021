19c019d0f6e083225a5f5bf69d8bea62ab4790b9
Borja Badía <44509208+Borja92@users.noreply.github.com>
2021-02-11 20:35:48 +0100

    up

--- Practicas Segundo Trimestre/Collections/ContadorPalabrasDistintas/src/ContadorPalabrasDistintas.java
+++ Practicas Segundo Trimestre/Collections/ContadorPalabrasDistintas/src/ContadorPalabrasDistintas.java
@@ -4,34 +4,26 @@ import java.util.*;
 
 public class ContadorPalabrasDistintas {
 
-private TreeSet<String> formaFinal = new TreeSet<>();
+private HashSet<String> formaFinal = new HashSet<>();
     private String texto="Escribir un programa en Java que extraiga de un texto todas las palabras (Utilizando expresiones regulares) .\n" +
             "Se desea conocer cuántas palabras distintas existen en el texto dado, para ello, se deberán almacenar  y mostrar las palabras distintas (no repetidas) extraídas del texto en una estructura collection. \n" +
             "También se desea conocer el número de palabras distintas. Pingüino, Año";
     private int  numPalabrasDistintas=0;
-    private StringTokenizer st;
+    private StringTokenizer st1, st2;
 
     String normalizarTexto(){
         String textoFiltrado = texto;//Normalizer.normalize(texto, Normalizer.Form.NFD);
   textoFiltrado=   textoFiltrado.toLowerCase().replaceAll("[^\\dA-Za-z0-9Á-Úá-úüñ]"," ");
-        st= new StringTokenizer(textoFiltrado);
+        st1= new StringTokenizer(textoFiltrado);
 
-        String[] textoEnArray = new String[textoFiltrado.length()];
+        String[] textoEnArray = textoFiltrado.split("[ ]+");
 
-        for (int i = 0; i <textoFiltrado.length(); i++) {
 
-            formaFinal.add(textoFiltrado);
+        for (String s:textoEnArray ) {
+            formaFinal.add(s);
         }
-      //  textoEnArray.(textoFiltrado.split("[ ]+"));
-
-       // Iterator <String> it=textoEnArray.iterator();
-
-  /*      while (it.hasNext()){
-
-            formaFinal.add(it.next());
-        }
-*/
-        return "Número de palabras: "+st.countTokens()+" en el texto normalizado: "+formaFinal;
+        st2= new StringTokenizer(formaFinal.toString());
+        return "Número de palabras totales: "+st1.countTokens()+". Número de palabras quitando las repetidas: "+st2.countTokens()+ " en el texto normalizado: \n"+formaFinal;
     }
 
 
